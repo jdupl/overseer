a38020cfb93e2adc468089b5692bd247890bfe0b
Justin Duplessis <drfoliberg@gmail.com>
2015-06-15 15:09:08 -0400

    Add cli args and start using tinytag

--- overseer.py
+++ overseer.py
@@ -1,42 +1,65 @@
-import os
+import sys, getopt, os
 from tinytag import TinyTag
 
 def is_new_file(file, old_files):
     # TODO handle relative paths
-    return file in old_files
+    return file not in old_files
 
 def get_old_files():
     # TODO get actual files from last run
     return []
 
-def get_files(path):
+def get_files(path, extension):
     path_files = []
 
     for root, sub_folders, files in os.walk(path):
          for file in files:
-             path_files.append(file)
+            if not extension or file.endswith('.' + extension):
+                 path_files.append(root + '/' + file)
     return path_files
 
-def get_lossless_files(files):
-    lossless_files = []
+def help():
+    print('overseer.py -s <source> -d <destination>')
 
-    for file in files:
-        if file.endswith('.mp3'):
-            lossless_files.append(file)
-    return lossless_files
 
-def main():
-    source = "Téléchargements"
-    output = "output"
+def main(argv):
+    source = None
+    destination = None
+    try:
+        opts, args = getopt.getopt(argv, "hs:d:",["source=","destination="])
+    except getopt.GetoptError:
+        help()
+        sys.exit(1)
+    for opt, arg in opts:
+        if opt == '-h':
+            help()
+            sys.exit(0)
+        elif opt in ("-s", "--source"):
+            source = arg
+        elif opt in ("-d", "--destination"):
+            destination = arg
 
+    if not destination or not source:
+        help()
+        sys.exit(1)
+
+    to_encode = []
     old_files = get_old_files()
-    source_files = get_lossless_files(get_files(source))
+    output_files = get_files(destination, 'opus')
+    source_files = get_files(source, 'flac')
 
     for file in source_files:
         if is_new_file(file, old_files):
-            tags = TinyTag.get(file)
-            print(tags.artist)
-            print(tags.album)
+            to_encode.append(file)
+
+    for file in to_encode:
+        tags = TinyTag.get(file)
+        album_name = tags.album
+        # TODO pad with 0 for 2 length
+        track_file = tags.track + ' - ' + tags.title
+        track_relative_path = tags.album + '/' + track_name
+        track_absolute_path = output + '/' + track_relative_path
 
 
-main()
+if __name__ == "__main__":
+   main(sys.argv[1:])
